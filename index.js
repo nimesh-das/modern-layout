8e87365b72e4365ee5595f954b5b80af3834419e
nimesh-das <ndas@uwaterloo.ca>
2020-11-20 16:50:07 -0500

    re-added jquery

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+import jquery from "jquery";
+window.$ = window.jQuery = jquery;
+import $ from "jquery";
+import { fullpage } from './jquery.fullPage.js';
+import './jquery.fullPage.css';
+import Highway from '@dogstudio/highway';
+import Fade from './transition';
+
+$("#wrapper").fullpage({
+ 	anchors:['ex1', 'ex2', 'ex3', 'ex4']
+ })
+
 const H = new Highway.Core({
 	transitions: {
 		default: Fade
 	}
 });
+
+console.log("Hello world")
